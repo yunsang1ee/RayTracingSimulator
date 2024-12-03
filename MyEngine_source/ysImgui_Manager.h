a0816a2f793806e1f2757bd1538d28ac207be9e6
이윤상 <kwjdduf88@gmail.com>
2024-12-03 14:00:34 +0900

    imgui file directory move

--- MyEngine_source/ysImgui_Manager.h
+++ MyEngine_source/ysImgui_Manager.h
@@ -2,10 +2,10 @@
 #include "CommonInclude.h"
 
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include "imgui_impl_opengl3_loader.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_opengl3_loader.h"
 
 namespace ys
 {
